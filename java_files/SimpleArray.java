a3b6b1535424aa3326883c803b1123dbf017812b
Logesh <logeshsakthi2005@gmail.com>
2025-11-27 18:10:22 +0530

    Add files via upload

---
+++ java_files/SimpleArray.java
@@ -0,0 +1,9 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+        
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at " + i + ": " + numbers[i]);
+        }
+    }
+}
